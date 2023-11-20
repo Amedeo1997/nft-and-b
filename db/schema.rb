a0745f4e3fe61f37da9416e5d289bacace5b8d6a
ahmed salhi <sirjaizz@gmail.com>
2023-11-20 16:05:48 +0100

    problem solved I hope

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 ActiveRecord::Schema[7.1].define(version: 2023_11_20_143216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  create_table "reservations", force: :cascade do |t|
-    t.boolean "status", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -31,6 +25,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_143216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
