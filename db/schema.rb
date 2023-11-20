58dfc21489a2f7e8157bbc3c1c1d0fe86f47812a
alexandra perrin <alexandrarose1@live.com.au>
2023-11-20 16:19:45 +0100

    new push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_143216) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_150246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,8 +29,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_143216) do
 
   create_table "reservations", force: :cascade do |t|
     t.boolean "status", default: false
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reservations_on_product_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_143216) do
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "reservations", "products"
+  add_foreign_key "reservations", "users"
 end
