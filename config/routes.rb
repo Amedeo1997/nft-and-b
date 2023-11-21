b42e237a9a5ffdf51ce870301d0370c9193b8b15
ahmed salhi <sirjaizz@gmail.com>
2023-11-21 14:54:03 +0100

    user route and controller and views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-    resources :products, only: [:create, :new, :delete]
+  resources :products, only: [:create, :new, :delete]
+  resources :users, only: [:new, :create, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
